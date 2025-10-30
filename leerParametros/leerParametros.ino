7659705aea133ea7118f39f3c77f33321863b87f
Franco N. Angeletti <franco.angeletti01@gmail.com>
2025-10-30 13:01:17 -0300

    Manejo de parametros desde una estructura en lugar de distribuir listas

--- leerParametros/leerParametros.ino
+++ leerParametros/leerParametros.ino
@@ -31,28 +31,34 @@ LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 // Instancias para la lectura de cada PZEM (R, S y T)
 PZEM004Tv30 pzems[] = {
-  PZEM004Tv30 (2, 3, 0x00), // Rx y Tx de Arduino, cruzados para cada PZEM004T y, el último parámetro es la dirección usada en MODBUS... No la uso
+  PZEM004Tv30 (2, 3, 0x00), // Pin de recepción y transmisión de arduino.
   PZEM004Tv30 (4, 5, 0x00),
   PZEM004Tv30 (6, 7, 0x00)
 };
 
-// Parámetros y métodos
-const char* medidas[]  = { "CORRIENTE", "POTENCIA", "ENERGIA", "VOLTAJE", "FRECUENCIA", "FACTOR DE POTENCIA" };
-const char* unidades[] = { "A",         "W",        "kWh",     "V",       "Hz",         " " };
+// Estructura usada para definir los parámetros a medir, su nombre y unidad
+typedef struct {
+  const char nombre[18];
+  const char unidad[5];
+  bool esAcumulativo; // Si es verdadero, se suma este parámetro en TODOS los sensores en lugar de mostrar el parametro de cada uno
+  float (PZEM004Tv30::*metodo)(); // Método de la librería pzem para obtener el parámetro especificado
+} ParametroElectrico;
 
-typedef float (PZEM004Tv30::*PZEMMethod)();
-PZEMMethod metodos[] = {
-	&PZEM004Tv30::current, &PZEM004Tv30::power,     &PZEM004Tv30::energy,
-	&PZEM004Tv30::voltage, &PZEM004Tv30::frequency, &PZEM004Tv30::pf
+// Lista de parámetros a medir en cada PZEM
+const ParametroElectrico ParametrosElectricos[] = {
+  { "VOLTAJE",            "V",   false, &PZEM004Tv30::voltage },
+  { "CORRIENTE",          "A",   false, &PZEM004Tv30::current },
+  { "FRECUENCIA",         "Hz",  false, &PZEM004Tv30::frequency },
+  { "POTENCIA",           "W",   true,  &PZEM004Tv30::power   },  
+  { "ENERGIA",            "kWh", true,  &PZEM004Tv30::energy },  
+  { "FACTOR DE POTENCIA", " ",   false, &PZEM004Tv30::pf },
 };
-
-// Enteros para recorrer las listas de parámetros eléctricos
-uint8_t indice = 0;
-const uint8_t cantidadParam = 6;
 
 // Intervalo de cambio de parámetros (ms)
 const unsigned long INTERVALO = 5000UL;
 unsigned long ultimoCambio = 0UL;
+
+uint8_t indiceParametros = 0; // Usado para ir variando los parámetros eléctricos a mostrar
 
 
 void setup() {
@@ -62,20 +68,19 @@ void setup() {
   // Inicializamos la instancia de la pantalla con la dirección del header
   lcd.init();
   lcd.backlight();
-
-  //delay(200);
 }
 
 void loop() {
 	// Si ya pasó el intervalo, mostramos la siguiente medición
 	if (millis() - ultimoCambio >= INTERVALO) {
-		mostrarLectura(indice);
+		mostrarLectura(indiceParametros);
    
 		// Ajusto el último intervalo hecho según lo que se tardó en escribir en el lcd
 		ultimoCambio = millis();
 
-		// Siguiente parámetro y retorno al rango 0–5
-		indice = (indice + 1) % cantidadParam;
+    // Recorro la lista de parámetros como un "vector circular"
+		size_t cantidadParametrosElectricos = sizeof(ParametrosElectricos) / sizeof(ParametroElectrico);
+		indiceParametros = (indiceParametros + 1) % cantidadParametrosElectricos;
 	}
 }
 
@@ -87,18 +92,19 @@ void mostrarLectura(uint8_t id) {
 
 	// Título, ¿Qué parámetro se muestra?
 	lcd.setCursor(0, 0);
-	lcd.print('['); lcd.print(medidas[id]); lcd.print(']');
+	lcd.print('['); lcd.print(ParametrosElectricos[id].nombre); lcd.print(']');
 
-  if (id == 1 || id == 2) { // POTENCIA ó ENERGÍA, se muestra la suma
+   // POTENCIA ó ENERGÍA, se muestra la suma
+  if (ParametrosElectricos[id].esAcumulativo) {
     
     // Leo cada PZEM y voy sumando su lectura de potencia o energía
     for (auto& pzem : pzems)
-      auxiliar += (pzem.*metodos[id])();
+      auxiliar += (pzem.*ParametrosElectricos[id].metodo)();
 
     // Etiqueta, "Se está mostrando la suma, no las individuales"
     lcd.setCursor(0, 1); lcd.print("R + S + T");
     // En la última línea, muestro el resultado de la suma con 1 decimal de precisión y su unidad
-    lcd.setCursor(0, 3); lcd.print(auxiliar, 1); lcd.print(" "); lcd.print(unidades[id]);
+    lcd.setCursor(0, 3); lcd.print(auxiliar, 1); lcd.print(" "); lcd.print(ParametrosElectricos[id].unidad);
     return;
   }
 
@@ -108,13 +114,13 @@ void mostrarLectura(uint8_t id) {
   for (uint8_t i = 0; i < cantidadSensores; i++) { // NOTA; Acá recorremos por índice para usar el número de 'i'
 
     // Medida de uno del parámetro de uno de los PZEM
-    auxiliar = (pzems[i].*metodos[id])();
+    auxiliar = (pzems[i].*ParametrosElectricos[id].metodo)();
 
     // Desde la fila 1 hasta la 3, vamos imprimiendo
     lcd.setCursor(0, i + 1);
     // El nombre de la línea (NOTA; Usamos ASCII para obtener las letras siguientes R->S->T)
     lcd.print((char)('R' + i)); lcd.print(": ");
     // Mostramos el valor con 1 decimal de precisión y su unidad
-    lcd.print(auxiliar, 1); lcd.print(" "); lcd.print(unidades[id]);
+    lcd.print(auxiliar, 1); lcd.print(" "); lcd.print(ParametrosElectricos[id].unidad);
   }
 }
