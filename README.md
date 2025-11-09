9fe8b0bdca74f90a2981bd3f7280acf3b897a0e0
Franco N. Angeletti <franco.angeletti01@gmail.com>
2025-11-09 20:11:22 -0300

    Agrego medida de temperatura y velocidad

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 ## Monitor de Potencia con Pantalla LCD I2C
 
-Este repositorio contiene dos programas para Arduino orientados al trabajo con pantallas LCD I2C (basadas en el chip **PCF8574**), junto a módulos de medición **PZEM004Tv30**. El objetivo es, poder ver parámetros eléctricos de corriente alterna (_trifásico_), en tiempo real.
+Este repositorio contiene dos programas para Arduino orientados al trabajo con pantallas LCD I2C (basadas en el chip **PCF8574**), junto a módulos de medición **PZEM004Tv30**. Permiten mostrar parámetros eléctricos trifásicos en un carrousel y, al finalizar la vuelta, una tarjeta extra con RPM (sensor Hall) y temperaturas.
 
 ### Estructura del repositorio
 
 ```
 .
 ├── leerParámetros/
-│   └── leerParámetros.ino # Muestra parámetros eléctricos en LCD I2C
+│   └── leerParámetros.ino # Muestra parámetros mencionados en LCD I2C
 ├── buscarPantalla/
 │   └── buscarPantalla.ino # Escanea el bus I2C para encontrar la dirección de la pantalla
 ```
@@ -17,16 +17,23 @@ Este repositorio contiene dos programas para Arduino orientados al trabajo con p
 - 1 Arduino UNO
 - 1 Pantalla LCD I2C 20x4 (PCF8574)
 - 3 Módulos PZEM004Tv30
+- 2 sensores de temperatura (salida analógica)
+- 1 sensor Hall (salida digital)
 
 ### Conexiones:
   - **LCD I2C**
     - SDA → A4
     - SCL → A5
-  - **PZEM004Tv30 1**: RX → D3, TX → D2
-  - **PZEM004Tv30 2**: RX → D5, TX → D4
-  - **PZEM004Tv30 3**: RX → D7, TX → D6
+  - **PZEM004Tv30 1**: RX → D5, TX → D4
+  - **PZEM004Tv30 2**: RX → D7, TX → D6
+  - **PZEM004Tv30 3**: RX → D9, TX → D8
+  - **Sensores de temperatura**:
+	- SENSOR 1: Salida Analógica → A0
+	- SENSOR 2: Salida Analógica → A1
+  - **Sensor Hall (efecto de campo)**:
+	- SALIDA DIGITAL → D2
 
-> Nota: Puede alterar los pines de cada PZEM por otro digital, al principio de ```leerParámetros/leerParámetros.ino``` pero, al estar usando Arduino UNO, la conexión con la pantalla I2C **debe ser sobre los pines nombrados**.
+> Nota: Puede alterar los pines de cada sensor por otro digital, al principio de ```leerParámetros/leerParámetros.ino``` pero, al estar usando Arduino UNO, la conexión con la pantalla I2C **debe ser sobre los pines nombrados**.
 ---
 
 ### Librerías necesarias
@@ -59,13 +66,17 @@ Este sketch muestra en la pantalla LCD los siguientes parámetros eléctricos de
 - Frecuencia en cada fase (Hz).
 - Factor de potencia de cada fase.
 
+Y, en una tarjeta adicional, luego de mostrar todos estos factores, se mostrará:
+- Temperatura de los dos sensores (°C).
+- Velocidad Angular tomada por el sensor Hall (RPM).
+
 ### Sumario: Cómo usar
 
 1. Usá `buscarPantalla.ino` para detectar la dirección I2C de tu pantalla.
 2. Modificá `leerParametros.ino` si es necesario para ajustar la dirección:
-   ```
-   LiquidCrystal_I2C lcd(0x20, 20, 4); // Cambiá 0x20 si tu pantalla tiene otra dirección
-   ```
+```
+LiquidCrystal_I2C lcd(0x20, 20, 4); // Cambiá 0x20 si tu pantalla tiene otra dirección
+```
 3. Cargá `leerParametros.ino` en tu Arduino.
 4. Conecta los sensores y pantallas como se nombró en [Conexiones](#Conexiones).
 5. Ahora, tu pantalla va a mostrar los datos que mida cada sensor.
--- leerParametros/leerParametros.ino
+++ leerParametros/leerParametros.ino
@@ -12,11 +12,18 @@
  *   SCL -> A5
 
  * Módulos PZEM004T:
- *   PZEM 1: RX -> D3, TX -> D2
- *   PZEM 2: RX -> D5, TX -> D4
- *   PZEM 3: RX -> D7, TX -> D6
+ *   PZEM 1: RX -> D5, TX -> D4
+ *   PZEM 2: RX -> D7, TX -> D6
+ *   PZEM 3: RX -> D9, TX -> D8
 
- * Nota: Las conexiones de cada sensor PZEM puede ser alterada en la instancia 'pzemX'.
+ * Sensores de Temperatura:
+ *   SENSOR 1: SALIDA ANALÓGICA -> A0
+ *   SENSOR 2: SALIDA ANALÓGICA -> A1
+
+ * Sensor de Efecto de Campo (Hall):
+ *   SENSOR: SALIDA DIGITAL -> D2
+
+ * Nota: Las conexiones de cada sensor puede ser alterada.
  * La pantalla no depende de estos módulos, sino de Wire.h, su pin no puede ser alterado en Arduino UNO.
  */
 #include <LiquidCrystal_I2C.h>
@@ -30,10 +37,11 @@
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 // Instancias para la lectura de cada PZEM (R, S y T)
+// RX, TX de Arduino
 PZEM004Tv30 pzems[] = {
-  PZEM004Tv30 (2, 3, 0x00), // Pin de recepción y transmisión de arduino.
   PZEM004Tv30 (4, 5, 0x00),
-  PZEM004Tv30 (6, 7, 0x00)
+  PZEM004Tv30 (6, 7, 0x00),
+  PZEM004Tv30 (8, 9, 0x00)
 };
 
 // Estructura usada para definir los parámetros a medir, su nombre y unidad
@@ -60,9 +68,23 @@ unsigned long ultimoCambio = 0UL;
 
 uint8_t indiceParametros = 0; // Usado para ir variando los parámetros eléctricos a mostrar
 
-uint8_t CANTIDAD_PARAMETROS;
-uint8_t CANTIDAD_SENSORES;
+uint8_t CANTIDAD_PARAMETROS_ELECTRICOS;
+uint8_t CANTIDAD_SENSORES_ELECTRICOS;
 
+/* ------------------ Sensores: temperatura y Hall ------------------ */
+// Pines de temperatura (analógicos)
+const int SEN_TEMP_1 = A0;
+const int SEN_TEMP_2 = A1;
+const float V_REF = 5.0;
+const int PASOS_ADC = 1024; // 2^10, con 10 los bits de presición del ADC de Arduino UNO
+
+// Sensor Hall en pin D2
+const uint8_t SEN_HALL_PIN = 2;
+volatile unsigned long contPulsos = 0; // Cuenta las veces que se recibió una interrupción por el sensor Hall
+
+
+void contadorPulsos() { contPulsos++; }
+void tarjetaExtra();
 
 void setup() {
   // Debemos iniciar la instancia de wire para poder ejecutar CrystalI2C
@@ -73,9 +95,13 @@ void setup() {
   lcd.backlight();
   
   // Cuento la cantidad parámetros y sensores dados
-  CANTIDAD_PARAMETROS = sizeof(ParametrosElectricos) / sizeof(ParametroElectrico);
-  CANTIDAD_SENSORES = sizeof(pzems) / sizeof(PZEM004Tv30);
+  CANTIDAD_PARAMETROS_ELECTRICOS = sizeof(ParametrosElectricos) / sizeof(ParametroElectrico);
+  CANTIDAD_SENSORES_ELECTRICOS = sizeof(pzems) / sizeof(PZEM004Tv30);
 
+  // Configuro pin del Hall (pin 2) y attachInterrupt en FALLING
+  // Usamos INPUT_PULLUP para tener estado alto estable y detectar la bajada del sensor
+  pinMode(SEN_HALL_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(SEN_HALL_PIN), contadorPulsos, FALLING);
 }
 
 void loop() {
@@ -87,7 +113,10 @@ void loop() {
 		ultimoCambio = millis();
 
     // Recorro la lista de parámetros como un "vector circular"
-		indiceParametros = (indiceParametros + 1) % CANTIDAD_PARAMETROS;
+		indiceParametros = (indiceParametros + 1) % CANTIDAD_PARAMETROS_ELECTRICOS;
+
+    // Si se terminó de mostrar los parámetros, mostramos una última tarjeta, la extra
+    if (!indiceParametros) tarjetaExtra();
 	}
 }
 
@@ -117,7 +146,7 @@ void mostrarLectura(uint8_t id) {
 
 
   // Si no se quiere leer potencia o energía, mostramos cada parámetro individual
-  for (uint8_t i = 0; i < CANTIDAD_SENSORES; i++) { // NOTA; Acá recorremos por índice para usar el número de 'i'
+  for (uint8_t i = 0; i < CANTIDAD_SENSORES_ELECTRICOS; i++) { // NOTA; Acá recorremos por índice para usar el número de 'i'
 
     // Medida de uno del parámetro de uno de los PZEM
     auxiliar = (pzems[i].*ParametrosElectricos[id].metodo)();
@@ -129,4 +158,37 @@ void mostrarLectura(uint8_t id) {
     // Mostramos el valor con 1 decimal de precisión y su unidad
     lcd.print(auxiliar, 1); lcd.print(" "); lcd.print(ParametrosElectricos[id].unidad);
   }
+}
+
+void tarjetaExtra() {
+  // A partir de este momento, vale lo que cuente el sensor Hall para hacer el cálculo
+  noInterrupts(); contPulsos = 0; interrupts();  
+  unsigned long tiempoActual;
+  
+  // Espera activa a que termine el tiempo de la tarjeta anterior (Permitimos que se suspenda por si SoftwareSerial tiene que cambiar de instancia y necesita sincronizarse)
+  while((tiempoActual = millis()) - ultimoCambio < INTERVALO) { delay(10); }
+
+  // Inicio de Sección crítica, no se medirá más al sensor Hall
+  noInterrupts();
+  // RPM = frecuencia x 60seg, en nuestro caso, usamos milisegundos para verificar si se excedio el tiempo del intervalo
+  // NOTA; Asumimos hay un solo imán en el eje, en caso de haber más, variamos la frecuencia.
+  unsigned long rpms = (contPulsos * 60000.0) / (tiempoActual - ultimoCambio);
+  // Salida inmediata de sección crítica
+  interrupts();
+  
+  // Leemos los valores "crudos" de cada sensor
+  int rawTemp1 = analogRead(SEN_TEMP_1);
+  int rawTemp2 = analogRead(SEN_TEMP_2);
+
+  // Los sensores de temperatura que usamos tienen una resolución de 0°C a 100°C con un factor lineal de 10mV/°C (Y, 0°C = 0V)
+  float temperatura1 = ((V_REF / PASOS_ADC) * rawTemp1) / 0.01; // 10mV/°C = 0.01V/°C
+  float temperatura2 = ((V_REF / PASOS_ADC) * rawTemp2) / 0.01;
+
+  // A partir de acá, solamente mostramos por el LCD los datos tomados
+  lcd.clear();
+  lcd.setCursor(0, 0); lcd.print("[VELOCIDAD ANGULAR]"); lcd.setCursor(0, 1); lcd.print(rpms);
+  lcd.setCursor(0, 2); lcd.print("[Tem.Rod.1] "); lcd.print(temperatura1, 1); lcd.print(" C");
+  lcd.setCursor(0, 3); lcd.print("[Tem.Rod.2] "); lcd.print(temperatura2, 1); lcd.print(" C");
+
+  ultimoCambio = millis(); // Actualizamos el "nuevo" último cambio y nos vamos ;)
 }
